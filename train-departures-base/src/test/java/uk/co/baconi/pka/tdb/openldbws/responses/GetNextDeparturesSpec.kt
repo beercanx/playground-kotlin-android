83fef85069198bef28baf5c4b5032c77eed06979
James Bacon <james@baconi.co.uk>
2019-01-19 11:26:20 +0000

    [UPDATED] GetNextDepartures test to properly validate the data

--- train-departures-base/src/test/java/uk/co/baconi/pka/tdb/openldbws/responses/GetNextDeparturesSpec.kt
+++ train-departures-base/src/test/java/uk/co/baconi/pka/tdb/openldbws/responses/GetNextDeparturesSpec.kt
@@ -1,5 +1,8 @@
 package uk.co.baconi.pka.tdb.openldbws.responses
 
+import io.kotlintest.assertSoftly
+import io.kotlintest.matchers.string.shouldContain
+import io.kotlintest.shouldBe
 import io.kotlintest.shouldNotBe
 import io.kotlintest.specs.StringSpec
 
@@ -17,6 +20,35 @@ class GetNextDeparturesSpec : StringSpec({
 
         result shouldNotBe null
 
-        // TODO - Verify data in objects based on test resource
+        assertSoftly {
+
+            val departuresBoard = result?.body?.getNextDeparturesResponse?.departuresBoard
+            departuresBoard?.generatedAt shouldBe "2019-01-13T13:51:17.106902+00:00"
+            departuresBoard?.locationName shouldBe "Sheffield"
+            departuresBoard?.crs shouldBe "SHF"
+            departuresBoard?.platformAvailable shouldBe true
+
+            departuresBoard?.nrccMessages?.messages?.first() shouldContain("Disruption between Bristol Temple Meads and Taunton via Weston-super-Mare")
+
+            val destinations = departuresBoard?.departures?.destination
+            destinations?.size shouldBe 1
+            destinations?.first()?.crs shouldBe "CLE"
+
+            val service = destinations?.first()?.service
+            service?.sta shouldBe "14:08"
+            service?.eta shouldBe "On time"
+            service?.std shouldBe "14:29"
+            service?.etd shouldBe "On time"
+            service?.platform shouldBe "1B"
+            service?.operator shouldBe "TransPennine Express"
+            service?.operatorCode shouldBe "TP"
+            service?.serviceType shouldBe "train"
+            service?.serviceID shouldBe "8ipq5Cv9fDMbR0rDg6riKA=="
+            service?.rsid shouldBe "TP603200"
+            service?.origin?.locationName shouldBe "Manchester Airport"
+            service?.origin?.crs shouldBe "MIA"
+            service?.destination?.locationName shouldBe "Cleethorpes"
+            service?.destination?.crs shouldBe "CLE"
+        }
     }
 })
