7afdff3e4f4f08f5d40433c0dd704cc74471860d
James Bacon <james.bacon@skybettingandgaming.com>
2019-01-25 12:12:58 +0000

    [ADDED] Generated settings page and in a crude way to render.

--- train-departures-android/src/main/java/uk/co/baconi/pka/td/DepartureSearchActivity.kt
+++ train-departures-android/src/main/java/uk/co/baconi/pka/td/DepartureSearchActivity.kt
@@ -1,18 +1,27 @@
 package uk.co.baconi.pka.td
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 
-import kotlinx.android.synthetic.main.activity_departure_search.*
+import kotlinx.android.synthetic.main.activity_departure_search.fab
+import kotlinx.android.synthetic.main.activity_departure_search.settings
+import kotlinx.android.synthetic.main.activity_departure_search.toolbar
+
 import uk.co.baconi.pka.tdb.StationCodes
 
 class DepartureSearchActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_departure_search)
         setSupportActionBar(toolbar)
+
+        settings.setOnClickListener { view ->
+            startActivity(Intent(this, SettingsActivity::class.java))
+        }
 
         fab.setOnClickListener { view ->
             Snackbar
