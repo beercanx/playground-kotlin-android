c0bad34461c6591a1a7a7b43bf5904b88013beed
James Bacon <james@baconi.co.uk>
2019-06-22 16:15:36 +0100

    [UPDATED] Fixed issue with toolbar not being initialised in settings.

--- train-departures-android/src/main/java/uk/co/baconi/pka/td/settings/SettingsActivity.kt
+++ train-departures-android/src/main/java/uk/co/baconi/pka/td/settings/SettingsActivity.kt
@@ -2,6 +2,7 @@ package uk.co.baconi.pka.td.settings
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.content_app_bar_layout.*
 import uk.co.baconi.pka.td.R
 
 class SettingsActivity : AppCompatActivity() {
@@ -9,6 +10,7 @@ class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+        setSupportActionBar(toolbar)
         supportFragmentManager
             .beginTransaction()
             .replace(R.id.settings_container, SettingsFragment())
