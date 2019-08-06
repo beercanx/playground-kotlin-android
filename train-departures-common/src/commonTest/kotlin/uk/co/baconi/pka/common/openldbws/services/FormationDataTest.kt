165e861fa6585d8293cbf5ccdadcb35ab3c9bd6a
James Bacon <james@baconi.co.uk>
2019-08-06 18:47:44 +0100

    [ADDED] Tests for formation data.

--- train-departures-common/src/commonTest/kotlin/uk/co/baconi/pka/common/openldbws/services/FormationDataTest.kt
+++ train-departures-common/src/commonTest/kotlin/uk/co/baconi/pka/common/openldbws/services/FormationDataTest.kt
@@ -1,5 +1,24 @@
 package uk.co.baconi.pka.common.openldbws.services
 
-class FormationDataTest {
-    // TODO
+import uk.co.baconi.pka.common.BaseDtoTest
+import uk.co.baconi.pka.common.openldbws.services.FormationData.Companion.formationData
+import uk.co.baconi.pka.common.xml.XmlDeserializer
+import kotlin.test.Test
+
+class FormationDataTest : BaseDtoTest<FormationData> {
+
+    override val tag: String = "formation"
+    override fun XmlDeserializer.extractor(): FormationData = formationData()
+
+    @Test
+    fun `Should decode with no fields present`() {
+        XmlDeserializer("<$tag/>").extractor() shouldBe FormationData()
+        XmlDeserializer("<$tag></$tag>").extractor() shouldBe FormationData()
+    }
+
+    @Test
+    fun `Should decode with each field present`() {
+        decodeAndGetField("avgLoading", 90, FormationData::averageLoading) shouldBe 90
+        decodeAndGetField("coaches", "", FormationData::coaches) shouldBe emptyList()
+    }
 }
