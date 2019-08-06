6d8e12e940a129c057bd7dab50a0e3be2da03f51
James Bacon <james@baconi.co.uk>
2019-08-06 19:00:31 +0100

    [ADDED] Tests for coach data.

--- train-departures-common/src/commonTest/kotlin/uk/co/baconi/pka/common/openldbws/services/CoachDataTest.kt
+++ train-departures-common/src/commonTest/kotlin/uk/co/baconi/pka/common/openldbws/services/CoachDataTest.kt
@@ -1,5 +1,40 @@
 package uk.co.baconi.pka.common.openldbws.services
 
-class CoachDataTest {
-    // TODO
+import uk.co.baconi.pka.common.BaseDtoTest
+import uk.co.baconi.pka.common.openldbws.services.CoachData.Companion.coach
+import uk.co.baconi.pka.common.openldbws.services.CoachData.Companion.coaches
+import uk.co.baconi.pka.common.xml.XmlDeserializer
+import kotlin.test.Test
+
+class CoachDataTest : BaseDtoTest<CoachData> {
+
+    override val tag: String = "coach"
+    override fun XmlDeserializer.extractor(): CoachData = coach()
+
+    @Test
+    fun `Should decode with no fields present`() {
+        XmlDeserializer("<$tag/>").extractor() shouldBe CoachData()
+        XmlDeserializer("<$tag></$tag>").extractor() shouldBe CoachData()
+    }
+
+    @Test
+    fun `Should decode with each field present`() {
+        decodeAndGetField("coachClass", "First", CoachData::coachClass) shouldBe "First"
+        decodeAndGetField("loading", 90, CoachData::loading) shouldBe 90
+        decodeAndGetField("number", "A", CoachData::number) shouldBe "A"
+        decodeAndGetField("toilet", "", CoachData::toilet) shouldBe Toilet()
+    }
+
+    @Test
+    fun `Should decode with no coaches present`() {
+        XmlDeserializer("<coaches/>").coaches() shouldBe emptyList()
+        XmlDeserializer("<coaches></coaches>").coaches() shouldBe emptyList()
+    }
+
+    @Test
+    fun `Should decode with coaches present`() {
+        XmlDeserializer("<coaches><coach/></coaches>").coaches() shouldBe listOf(CoachData())
+        XmlDeserializer("<coaches><coach></coach></coaches>").coaches() shouldBe listOf(CoachData())
+        XmlDeserializer("<coaches><coach/><coach/></coaches>").coaches() shouldBe listOf(CoachData(), CoachData())
+    }
 }
