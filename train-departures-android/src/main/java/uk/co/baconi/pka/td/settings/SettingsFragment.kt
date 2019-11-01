06781e10389057164fd111cb5acaf39bb152e94c
James Bacon <james@baconi.co.uk>
2019-11-01 07:51:59 +0000

    [ADDED] Breaking chnage to remind myself where to pick up again.

--- train-departures-android/src/main/java/uk/co/baconi/pka/td/settings/SettingsFragment.kt
+++ train-departures-android/src/main/java/uk/co/baconi/pka/td/settings/SettingsFragment.kt
@@ -2,10 +2,13 @@ package uk.co.baconi.pka.td.settings
 
 import android.os.Bundle
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.PreferenceScreen
 import uk.co.baconi.pka.td.R
 
 class SettingsFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-        setPreferencesFromResource(R.xml.pref_general, rootKey)
+        preferenceScreen = preferenceManager.createPreferenceScreen(context).apply {
+            // TODO - Create settings
+        }
     }
 }
