baad6b950f3d119c3d842fd3f81d320ab4858b9f
James Bacon <james@baconi.co.uk>
2019-11-01 08:42:26 +0000

    [REMOVED] Code that doesn't make sense beign in runTest block.

--- train-departures-common/src/jvmTest/kotlin/uk/co/baconi/pka/common/runTest.kt
+++ train-departures-common/src/jvmTest/kotlin/uk/co/baconi/pka/common/runTest.kt
@@ -4,9 +4,6 @@ import kotlinx.coroutines.runBlocking
 import java.io.File
 
 actual fun <T> runTest(block: suspend () -> T) {
-
-    File("abc").endsWith("")
-
     runBlocking {
         block()
     }
